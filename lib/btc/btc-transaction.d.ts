906653b66a44041892031038c56174e31351c581
nguyenkhoa <nguyenkhoa72.work@gmail.com>
2022-08-31 09:47:36 +0700

    build for 1.6

--- lib/btc/btc-transaction.d.ts
+++ lib/btc/btc-transaction.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 export declare class RawTransaction {
     _tx: any;
     _isSegwit: boolean;
@@ -9,8 +10,18 @@ export declare class RawTransaction {
     toSignHex(vindex: number): string;
     toSegwitSignHex(vindex: number): string;
     sign(privKey: any, inputs: number[]): Promise<any>;
-    createSigHash(vindex: number, hashType: number): Promise<any>;
+    createSigHash(vindex: number, hashType: number, redeemScript?: Buffer): Promise<any>;
     fromHex(hex: string): void;
     deepCopy(tx: RawTransaction): void;
     genHashId(): string;
+}
+export declare class MultiSigTransaction extends RawTransaction {
+    _pubKeys: Array<Buffer>;
+    _n: number;
+    _m: number;
+    constructor(n: number, m: number, pubKeys: Array<string>, isSegwit?: boolean);
+    addMultiSigInput(address: string, txid: string, vout: number, signatures: Array<string>, amount?: string): void;
+    sign(privKey: any, inputs: number[]): Promise<any>;
+    multiSigScriptSig(sigs: Array<Buffer>): Buffer;
+    generateRedeemScript(): Buffer;
 }
